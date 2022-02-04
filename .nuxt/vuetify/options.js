8afc70648d405465120594542dde3c7d5e05f5e2
LucasSena <smartsena@gmail.com>
2022-02-04 13:39:50 -0300

    Removing first version of the CMR2_Panel prototype

--- .nuxt/vuetify/options.js
+++
@@ -1,1 +0,0 @@
-export default {"theme":{"light":true,"themes":{"light":{"primary":"#42a5f5","accent":undefined,"secondary":"#1976d2","info":"#26a69a","warning":"#ffc107","error":"#dd2c00","success":"#00e676"}}}}
--- plugins/i18n.js
+++
@@ -1,8 +0,0 @@
-export default ({ app }) => {
-    const { $moment } = app
-    $moment.locale(app.i18n.locale)
-
-    app.i18n.onLanguageSwitched = (_, newLocale) => {
-        $moment.locale(newLocale)
-    }
-}
