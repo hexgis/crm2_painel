19a03016b1b8f89cbce12c7ca93a6937d2e41721
Gabriel Mariano <gabrielmarianosf@gmail.com>
2022-06-01 14:49:59 -0300

    [FIX] fix import category prodes

--- components/support/SupportLayers.vue
+++ components/support/SupportLayers.vue
@@ -32,6 +32,7 @@ export default {
         'showFeatures',
         'supportLayersCategoryFire',
         'supportLayersCategoryRaster',
+        'supportLayersCategoryProdes',
     ]),
 }
 </script>
