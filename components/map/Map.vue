d5b38bf51b44b654b3e8bc250477c79dc1872d66
Gabriel Freitas <gabrielmarianosf@gmail.com>
2022-07-07 11:20:26 -0300

    [FIX] fix name main map and base maps

--- components/map/Map.vue
+++ components/map/Map.vue
@@ -257,7 +257,7 @@ export default {
         ],
         mapOptions: {
             zoomControl: false,
-            name: '1',
+            name: 'mainMap',
         },
         areaBounds: null,
         initialBounds: [
@@ -293,45 +293,45 @@ export default {
                     zIndex: 1,
                 },
             },
-            // {
-            //     url: '//{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-            //     options: {
-            //         label: 'Google Satellite',
-            //         tag: 'Google Satellite',
-            //         attribution:
-            //             'Map data &copy; <a href="//maps.google.com/">Google</a> sattelite imagery',
-            //         maxZoom: 21,
-            //         maxNativeZoom: 19,
-            //         subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-            //         zIndex: 1,
-            //     },
-            // },
-            // {
-            //     url: '//mt0.google.com/vt/lyrs=r&hl=en&x={x}&y={y}&z={z}',
-            //     options: {
-            //         label: 'Google Roadmap',
-            //         tag: 'Google Roadmap',
-            //         attribution:
-            //             'Map data &copy; <a href="//maps.google.com/">Google</a> Altered roadmap',
-            //         maxZoom: 21,
-            //         maxNativeZoom: 19,
-            //         subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-            //         zIndex: 1,
-            //     },
-            // },
-            // {
-            //     url: '//mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}',
-            //     options: {
-            //         label: 'Google Hybrid',
-            //         tag: 'Google Hybrid',
-            //         attribution:
-            //             'Map data &copy; <a href="//maps.google.com/">Google</a> Hybrid',
-            //         maxZoom: 21,
-            //         maxNativeZoom: 19,
-            //         subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-            //         zIndex: 1,
-            //     },
-            // },
+            {
+                url: '//{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+                options: {
+                    label: 'Google Satellite',
+                    tag: 'Google Satellite',
+                    attribution:
+                        'Map data &copy; <a href="//maps.google.com/">Google</a> sattelite imagery',
+                    maxZoom: 21,
+                    maxNativeZoom: 19,
+                    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                    zIndex: 1,
+                },
+            },
+            {
+                url: '//mt0.google.com/vt/lyrs=r&hl=en&x={x}&y={y}&z={z}',
+                options: {
+                    label: 'Google Roadmap',
+                    tag: 'Google Roadmap',
+                    attribution:
+                        'Map data &copy; <a href="//maps.google.com/">Google</a> Altered roadmap',
+                    maxZoom: 21,
+                    maxNativeZoom: 19,
+                    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                    zIndex: 1,
+                },
+            },
+            {
+                url: '//mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}',
+                options: {
+                    label: 'Google Hybrid',
+                    tag: 'Google Hybrid',
+                    attribution:
+                        'Map data &copy; <a href="//maps.google.com/">Google</a> Hybrid',
+                    maxZoom: 21,
+                    maxNativeZoom: 19,
+                    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                    zIndex: 1,
+                },
+            },
             // {
             //     url:
             //         '//securewatch.digitalglobe.com/earthservice/wmtsaccess?connectId={connectid}&SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetTile&TileMatrixSet=EPSG:3857&LAYER=DigitalGlobe:ImageryTileService&FORMAT=image/jpeg&STYLE=&featureProfile=Vivid_2019&TileMatrix=EPSG:3857:{z}&TILEROW={y}&TILECOL={x}',
@@ -346,18 +346,18 @@ export default {
             //         zIndex: 1,
             //     },
             // },
-            // {
-            //     url: '//{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-            //     options: {
-            //         label: 'CartoDB',
-            //         tag: 'CartoDB',
-            //         attribution:
-            //             'Map data &copy; <a href="//www.openstreetmap.org/">OpenStreetMap</a> contributors, CartoDB Imagery <a href="//creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-            //         maxZoom: 21,
-            //         maxNativeZoom: 19,
-            //         zIndex: 1,
-            //     },
-            // },
+            {
+                url: '//{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+                options: {
+                    label: 'CartoDB',
+                    tag: 'CartoDB',
+                    attribution:
+                        'Map data &copy; <a href="//www.openstreetmap.org/">OpenStreetMap</a> contributors, CartoDB Imagery <a href="//creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+                    maxZoom: 21,
+                    maxNativeZoom: 19,
+                    zIndex: 1,
+                },
+            },
             // {
             //     url: '//view.geoapi-airbusds.com/maps/wmts/52a994d7-f215-4c66-aa10-439221c29ee0/tile/1.0.0/8659bd97-ea52-474d-a3e9-072c335cd6bb/default/3857/{z}/{y}/{x}',
             //     options: {
@@ -382,30 +382,30 @@ export default {
             //         zIndex: 1,
             //     },
             // },
-            // {
-            //     url: '//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-            //     options: {
-            //         label: 'ArcMap',
-            //         tag: 'ArcMap',
-            //         attribution:
-            //             'Map data &copy; <a href="//desktop.arcgis.com/en/arcmap/">ArcGis Basemap</a>',
-            //         maxZoom: 21,
-            //         maxNativeZoom: 19,
-            //         zIndex: 1,
-            //     },
-            // },
-            // {
-            //     url: 'https://tiles.planet.com/basemaps/v1/planet-tiles/planet_medres_visual_2020-10_mosaic/gmap/{z}/{x}/{y}.png?api_key=57cd3a8c44024cfdb7446ac37d8d1fe9',
-            //     options: {
-            //         label: 'Planet - Out/2020',
-            //         tag: 'Planet - Out/2020',
-            //         attribution:
-            //             'Map data &copy; <a href="//www.planet.com/">Planet</a>',
-            //         maxZoom: 21,
-            //         maxNativeZoom: 15,
-            //         zIndex: 1,
-            //     },
-            // },
+            {
+                url: '//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+                options: {
+                    label: 'ArcMap',
+                    tag: 'ArcMap',
+                    attribution:
+                        'Map data &copy; <a href="//desktop.arcgis.com/en/arcmap/">ArcGis Basemap</a>',
+                    maxZoom: 21,
+                    maxNativeZoom: 19,
+                    zIndex: 1,
+                },
+            },
+            {
+                url: 'https://tiles.planet.com/basemaps/v1/planet-tiles/planet_medres_visual_2020-10_mosaic/gmap/{z}/{x}/{y}.png?api_key=57cd3a8c44024cfdb7446ac37d8d1fe9',
+                options: {
+                    label: 'Planet - Out/2020',
+                    tag: 'Planet - Out/2020',
+                    attribution:
+                        'Map data &copy; <a href="//www.planet.com/">Planet</a>',
+                    maxZoom: 21,
+                    maxNativeZoom: 15,
+                    zIndex: 1,
+                },
+            },
         ],
         bingKey:
             'AuhiCJHlGzhg93IqUH_oCpl_-ZUrIE6SPftlyGYUvr9Amx5nzA-WqGcPquyFZl4L',
