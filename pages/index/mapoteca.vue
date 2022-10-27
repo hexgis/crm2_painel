dafb7d17af25bf05c202aceaa69a029a153ac20c
Gabriel Freitas <gabrielmarianosf@gmail.com>
2022-10-27 10:14:55 -0300

    [UPDATE] remove uncessary variable

--- pages/index/mapoteca.vue
+++ pages/index/mapoteca.vue
@@ -1,51 +1,61 @@
 <template>
-    <v-container class="d-flex flex-column">
-        <div class="tab-header flex justify-space-between">
-            <h4 class="subtitle-2 text-uppercase font-weight-regular">
-                {{ $t('title') }}
-            </h4>
-            <!-- <v-switch
+  <v-container class="d-flex flex-column">
+    <div class="tab-header flex justify-space-between">
+      <h4 class="subtitle-2 text-uppercase font-weight-regular">
+        {{ $t('title') }}
+      </h4>
+      <!-- <v-switch
                 v-if="features"
                 v-model="showFeaturesMonitoring"
                 class="mt-n1 ml-5"
                 hide-details
             /> -->
-        </div>
-        <v-card-text>
-            <p>
-                Ambiente de pesquisa de mapas.
-                Somente mapas são permitidos.
-            </p>
+    </div>
+    <v-card-text>
+      <p>
+        Ambiente de pesquisa de mapas.
+        Somente mapas são permitidos.
+      </p>
 
-            <v-list-item-content style="list-style: circle">
-                <v-list-item-title class="list__text mb-4 font-weight-black"
-                    >Relacionados:</v-list-item-title
-                >
-                <ul v-for="(item, index) in items" :key="index">
-                    <li>{{ item.value }}</li>
-                </ul>
-            </v-list-item-content>
-        </v-card-text>
-
-        <v-btn color="accent" class="" @click="showDialog(true)">
-            {{ $t('input-label') }}
-        </v-btn>
-        <div v-if="showDialogMapoteca">
-            <MapotecaDialog />
-        </div>
-        <v-footer
-            absolute
-            class="priority-footer"
-            color="#FFFFFF"
-            elevation="4"
+      <v-list-item-content style="list-style: circle">
+        <v-list-item-title class="list__text mb-4 font-weight-black">
+          Relacionados:
+        </v-list-item-title>
+        <ul
+          v-for="(item, index) in items"
+          :key="index"
         >
-            <v-col>
-                <v-row class="d-flex justify-center">
-                    <v-img max-width="200" src="/img/logocmr_normal.png" />
-                </v-row>
-            </v-col>
-        </v-footer>
-    </v-container>
+          <li>{{ item.value }}</li>
+        </ul>
+      </v-list-item-content>
+    </v-card-text>
+
+    <v-btn
+      color="accent"
+      class=""
+      @click="showDialog(true)"
+    >
+      {{ $t('input-label') }}
+    </v-btn>
+    <div v-if="showDialogMapoteca">
+      <MapotecaDialog />
+    </div>
+    <v-footer
+      absolute
+      class="priority-footer"
+      color="#FFFFFF"
+      elevation="4"
+    >
+      <v-col>
+        <v-row class="d-flex justify-center">
+          <v-img
+            max-width="200"
+            src="/img/logocmr_normal.png"
+          />
+        </v-row>
+      </v-col>
+    </v-footer>
+  </v-container>
 </template>
 
 <i18n>
@@ -66,34 +76,40 @@
 </i18n>
 
 <script>
-import MapotecaDialog from '@/components/mapoteca/MapotecaDialog'
-import { mapActions, mapMutations, mapState } from 'vuex'
-export default {
-    components: { MapotecaDialog },
-    data() {
-        return {
-            items: [
-                { value: 'Imagem de Alta Resolução ​' },
-                { value: 'Uso e Ocupação do Solo' },
-                { value: 'Uso e Ocupação com Imagem de Alta Resolução' },
-            ],
-        }
-    },
-    computed: {
-        ...mapState('mapoteca', [
-            'showDialogMapoteca',
-            'showFeatures',
-            'features',
-        ]),
-    },
-    methods: {
-        showDialog(value) {
-            this.setShowDialogMapoteca(value)
-        },
+import { mapMutations, mapState } from 'vuex';
+import MapotecaDialog from '@/components/mapoteca/MapotecaDialog';
 
-        ...mapMutations('mapoteca', ['setShowDialogMapoteca']),
+export default {
+  name: 'PageMapoteca',
+
+  components: { MapotecaDialog },
+
+  data() {
+    return {
+      items: [
+        { value: 'Imagem de Alta Resolução ​' },
+        { value: 'Uso e Ocupação do Solo' },
+        { value: 'Uso e Ocupação com Imagem de Alta Resolução' },
+      ],
+    };
+  },
+
+  computed: {
+    ...mapState('mapoteca', [
+      'showDialogMapoteca',
+      'showFeatures',
+      'features',
+    ]),
+  },
+
+  methods: {
+    showDialog(value) {
+      this.setShowDialogMapoteca(value);
     },
-}
+
+    ...mapMutations('mapoteca', ['setShowDialogMapoteca']),
+  },
+};
 </script>
 
 <style scoped>
