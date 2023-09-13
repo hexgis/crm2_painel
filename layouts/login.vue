ea926755df9245401136b303a9158d735d1471fc
joao.fonseca <joao.fonseca@hex360.com.br>
2023-09-13 18:23:31 -0300

    identing and fixing fonts

--- layouts/login.vue
+++ layouts/login.vue
@@ -9,10 +9,23 @@
         >
           <nuxt />
         </div>
-        <v-row class="rounded-b-xl red darken-2"> <v-col></v-col></v-row>
+        <v-row class="rounded-b-xl red darken-2">
+          <v-col />
+        </v-row>
         <v-row class="ma-3 py-4 helpBar">
-          <v-col flat cols="12" md="4" lg="3">
-            <v-card flat class="md-padding background-none" layout="row" layout-wrap layout-align="space-between">
+          <v-col
+            flat
+            cols="12"
+            md="4"
+            lg="3"
+          >
+            <v-card
+              flat
+              class="md-padding background-none"
+              layout="row"
+              layout-wrap
+              layout-align="space-between"
+            >
               <v-card-title class="cmr-v2-footer-h4 text-h6 text-md-h5">
                 {{ $t('we-are-help-title') }}
               </v-card-title>
@@ -23,8 +36,18 @@
               </v-card-subtitle>
             </v-card>
           </v-col>
-          <v-col cols="12" md="4" lg="3">
-            <v-card flat class="md-padding background-none" layout="row" layout-wrap layout-align="space-between">
+          <v-col
+            cols="12"
+            md="4"
+            lg="3"
+          >
+            <v-card
+              flat
+              class="md-padding background-none"
+              layout="row"
+              layout-wrap
+              layout-align="space-between"
+            >
               <v-card-title class="cmr-v2-footer-h4 text-h6 text-md-h5">
                 {{ $t('access-help-title') }}
               </v-card-title>
@@ -33,15 +56,28 @@
                   {{ $t('access-help-text') }}
                 </p>
                 <p class="footer-text-content link-footer">
-                  <a :href="helpLinks.solicitacaoAcessoCmr2" class="text-h6">
+                  <a
+                    :href="helpLinks.solicitacaoAcessoCmr2"
+                    class="text-h6"
+                  >
                     {{ $t('access-help-link') }}
                   </a>
                 </p>
               </v-card-subtitle>
             </v-card>
           </v-col>
-          <v-col cols="12" md="4" lg="3">
-            <v-card flat class="md-padding background-none" layout="row" layout-wrap layout-align="space-between">
+          <v-col
+            cols="12"
+            md="4"
+            lg="3"
+          >
+            <v-card
+              flat
+              class="md-padding background-none"
+              layout="row"
+              layout-wrap
+              layout-align="space-between"
+            >
               <v-card-title class="cmr-v2-footer-h4 text-h6 text-md-h5">
                 {{ $t('project-help-title') }}
               </v-card-title>
@@ -52,13 +88,24 @@
               </v-card-subtitle>
             </v-card>
           </v-col>
-          <v-col v-if="userLogged" cols="12" md="4" lg="3">
-            <v-card flat class="md-padding background-none" layout="row" layout-wrap layout-align="space-between">
-              <v-card-title class="cmr-v2-footer-h4 text-h6 text-md-h">
+          <v-col
+            v-if="userLogged"
+            cols="12"
+            md="4"
+            lg="3"
+          >
+            <v-card
+              flat
+              class="md-padding background-none"
+              layout="row"
+              layout-wrap
+              layout-align="space-between"
+            >
+              <v-card-title class="cmr-v2-footer-h4 text-h6 text-md-h5">
                 {{ $t('manual-help-title') }}
               </v-card-title>
               <v-card-subtitle>
-                <p class="footer-text-content link-footer footer text-h6">
+                <p class="footer-text-content link-footer text-h6">
                   <a :href="helpLinks.cmrManualUrl">
                     {{ $t('manual-help-link') }}
                   </a>
@@ -73,18 +120,17 @@
       app
       absolute
     >
-    <v-container
-        fluid>
-      <v-row
-        dense
-        class="justifiy-space-between align-center"
+      <v-container
+        fluid
       >
-        <v-col />
-        <v-col class="text-center white--text">
-        </v-col>
-        <v-spacer />
-      </v-row>
-
+        <v-row
+          dense
+          class="justifiy-space-between align-center"
+        >
+          <v-col />
+          <v-col class="text-center white--text" />
+          <v-spacer />
+        </v-row>
       </v-container>
     </v-footer>
   </v-app>
@@ -93,9 +139,9 @@
 <i18n>
 {
   "en": {
-    "manual-help-title": "View User Manual",
-    "manual-help-link": "User Manual",
-    "project-help-title": "Contact the Project",
+    "manual-help-title": "View user manual",
+    "manual-help-link": "User manual",
+    "project-help-title": "Contact the project",
     "project-help-link": "Contact us",
     "access-help-title": "Request access",
     "access-help-link": "Access here",
@@ -121,21 +167,21 @@
 export default {
   name: 'App',
 
+  data() {
+    return {
+      userLogged: true, // "dataUser.token",
+      helpLinks: {
+        cmrManualUrl: 'https://cmr.funai.gov.br/api/media/Manual/Mapa Interativo/CMR_Manual_2021_V1.7.1.pdf',
+        solicitacaoAcessoCmr2: 'https://cmr.funai.gov.br/app/#/solicitacao-acesso',
+        cmrProjectUrl: 'https://cmr.funai.gov.br/o-projeto/',
+        cmrTalkToUs: 'https://cmr.funai.gov.br/contato/',
+      },
+    };
+  },
+
   head: () => ({
     title: 'Skyviewer',
-    }),
-
-  data() {
-    return{
-      userLogged: true, //"dataUser.token",
-      helpLinks: {
-        "cmrManualUrl": "https://cmr.funai.gov.br/api/media/Manual/Mapa Interativo/CMR_Manual_2021_V1.7.1.pdf",
-        "solicitacaoAcessoCmr2": "https://cmr.funai.gov.br/app/#/solicitacao-acesso",
-        "cmrProjectUrl": "https://cmr.funai.gov.br/o-projeto/",
-        "cmrTalkToUs": "https://cmr.funai.gov.br/contato/",
-      }
-    }
-  }
+  }),
 };
 </script>
 
