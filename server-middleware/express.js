d9c3cbf0539c2a1eaa601a1ad7e12315bb2b9c95
Joao Fonseca <joao.fonseca@hex360.com.br>
2024-05-01 14:54:51 -0300

    new back end

---
+++ server-middleware/express.js
@@ -0,0 +1,13 @@
+// server-middleware/express.js
+
+const express = require('express');
+
+const app = express();
+
+// Configuração do CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
