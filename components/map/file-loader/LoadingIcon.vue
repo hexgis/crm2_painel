82c96d71787a5efbd4a119ea756a9ed41e6498eb
Marcos Silva <marcos.silva@hex360.com.br>
2024-07-26 14:09:58 -0300

    [feature] fixing loading indigenous land information

---
+++ components/map/file-loader/LoadingIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="loading"></div>
+</template>
+
+<script>
+export default {
+    name: 'LoadingIcon'
+}
+</script>
+
+<style scoped lang="sass">
+    #loading
+        width: 80px
+        height: 80px
+        border: 10px solid #f1f1f1
+        border-top: 10px solid red
+        margin: auto
+        border-radius: 50%
+        animation: spinner 1s linear infinite
+    
+    @keyframes spinner 
+        to 
+            transform: rotate(360deg)
+
+</style>
