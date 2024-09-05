980ccd5cb52d05499d6a5eb3d85f1bee4e48ed90
João Vitor Lima <joao.fonseca@hex360.com.br>
2024-09-05 17:31:03 -0300

    merge develop

--- components/support/SupportLayersGroupRaster.vue
+++ components/support/SupportLayersGroupRaster.vue
@@ -1,30 +1,41 @@
 <template>
-    <v-list-group v-if="group" v-show="checkSearchLayer()" v-model="value">
-        <template #activator>
-            <template v-if="activeLayerCount > 0">
-                <v-badge color="#b2b1b1" offset-x="-270" offset-y="10">
-                    <template v-slot:badge>
-                        {{ activeLayerCount }}
-                    </template>
-                </v-badge>
-            </template>
-            <v-list-item-content>
-                <v-list-item-title class="text-wrap">
-                    <span class="text-cursor">
-                        {{ group.name }}
-                    </span>
-                </v-list-item-title>
-            </v-list-item-content>
+  <v-list-group
+    v-if="group"
+    v-show="checkSearchLayer()"
+    v-model="value"
+  >
+    <template #activator>
+      <template v-if="activeLayerCount > 0">
+        <v-badge
+          color="#b2b1b1"
+          offset-x="-270"
+          offset-y="10"
+        >
+          <template #badge>
+            {{ activeLayerCount }}
+          </template>
+        </v-badge>
+      </template>
+      <v-list-item-content>
+        <v-list-item-title class="text-wrap">
+          <span class="text-cursor">
+            {{ group.name }}
+          </span>
+        </v-list-item-title>
+      </v-list-item-content>
 
-            <v-col v-if="group.name === 'Alta Resolução'">
-                <div>
-                    <v-tooltip>
-                        <template #activator="{ on }">
-                            <v-icon v-on="on" class="pb-4">
-                                mdi-information
-                            </v-icon>
-                        </template>
-                        <span>
+      <v-col v-if="group.name === 'Alta Resolução'">
+        <div>
+          <v-tooltip>
+            <template #activator="{ on }">
+              <v-icon
+                class="pb-4"
+                v-on="on"
+              >
+                mdi-information
+              </v-icon>
+            </template>
+            <span>
               {{ $t('mosaic-line-1') }}
               <br>
               {{ $t('mosaic-line-2') }}
@@ -34,38 +45,38 @@
               {{ $t('mosaic-line-4') }}
               <br>
             </span>
-                    </v-tooltip>
-                </div>
-            </v-col>
-        </template>
+          </v-tooltip>
+        </div>
+      </v-col>
+    </template>
 
-        <template v-if="!searchLayer">
-            <v-container class="py-0">
-                <v-list expand>
-                    <SupportLayersGroupItemRaster
-                        v-for="layer in orderedSupportLayers"
-                        :key="layer"
-                        :layer-id="layer"
-                    />
-                </v-list>
-            </v-container>
-        </template>
+    <template v-if="!searchLayer">
+      <v-container class="py-0">
+        <v-list expand>
+          <SupportLayersGroupItemRaster
+            v-for="layer in orderedSupportLayers"
+            :key="layer"
+            :layer-id="layer"
+          />
+        </v-list>
+      </v-container>
+    </template>
 
-        <template v-else>
-            <v-container class="py-0">
-                <v-list expand>
-                    <template v-for="layer in orderedSupportLayers">
-                        <SupportLayersGroupItemRaster
-                            v-if="includesLayers(layer)"
-                            :key="layer"
-                            :layer-id="layer"
-                        />
-                    </template>
-                </v-list>
-            </v-container>
-        </template>
-        <v-divider />
-    </v-list-group>
+    <template v-else>
+      <v-container class="py-0">
+        <v-list expand>
+          <template v-for="layer in orderedSupportLayers">
+            <SupportLayersGroupItemRaster
+              v-if="includesLayers(layer)"
+              :key="layer"
+              :layer-id="layer"
+            />
+          </template>
+        </v-list>
+      </v-container>
+    </template>
+    <v-divider />
+  </v-list-group>
 </template>
 <i18n>
   {
@@ -84,69 +95,69 @@
   }
 </i18n>
 <script>
-import { mapState, mapMutations } from 'vuex'
-import _ from 'lodash'
-import SupportLayersGroupItemRaster from '@/components/support/SupportLayersGroupItemRaster'
+import { mapState, mapMutations } from 'vuex';
+import _ from 'lodash';
+import SupportLayersGroupItemRaster from '@/components/support/SupportLayersGroupItemRaster';
 
 export default {
-    name: 'SupportLayersGroupRaster',
+  name: 'SupportLayersGroupRaster',
 
-    components: { SupportLayersGroupItemRaster },
+  components: { SupportLayersGroupItemRaster },
 
-    props: {
-        group: {
-            type: Object,
-            default: null,
-        },
+  props: {
+    group: {
+      type: Object,
+      default: null,
+    },
+  },
+
+  data: () => ({
+    value: null,
+  }),
+
+  computed: {
+    supportLayers() {
+      return this.supportCategoryGroupsRaster[this.group.id].layers;
     },
 
-    data: () => ({
-        value: null,
-    }),
-
-    computed: {
-        supportLayers() {
-            return this.supportCategoryGroupsRaster[this.group.id].layers
-        },
-
-        orderedSupportLayers() {
-            return _.sortBy(this.supportLayers, 'order')
-        },
-
-        activeLayers() {
-            return this.orderedSupportLayers.filter(
-                (layer) => this.supportLayersCategoryRaster[layer].visible
-            )
-        },
-
-        activeLayerCount() {
-            return this.activeLayers.length
-        },
-
-        ...mapState('supportLayers', [
-            'supportCategoryGroupsRaster',
-            'searchLayer',
-            'filteredLayersId',
-            'supportLayersCategoryRaster',
-        ]),
+    orderedSupportLayers() {
+      return _.sortBy(this.supportLayers, 'order');
     },
 
-    methods: {
-        checkSearchLayer() {
-            if (this.searchLayer) {
-                this.value = true
-            }
-            return true
-        },
-
-        includesLayers(id) {
-            return this.filteredLayersId.includes(id)
-        },
-
-        ...mapMutations('supportLayers', [
-            'setexpandAllLayers',
-            'setretractAllLayers',
-        ]),
+    activeLayers() {
+      return this.orderedSupportLayers.filter(
+        (layer) => this.supportLayersCategoryRaster[layer].visible,
+      );
     },
-}
+
+    activeLayerCount() {
+      return this.activeLayers.length;
+    },
+
+    ...mapState('supportLayers', [
+      'supportCategoryGroupsRaster',
+      'searchLayer',
+      'filteredLayersId',
+      'supportLayersCategoryRaster',
+    ]),
+  },
+
+  methods: {
+    checkSearchLayer() {
+      if (this.searchLayer) {
+        this.value = true;
+      }
+      return true;
+    },
+
+    includesLayers(id) {
+      return this.filteredLayersId.includes(id);
+    },
+
+    ...mapMutations('supportLayers', [
+      'setexpandAllLayers',
+      'setretractAllLayers',
+    ]),
+  },
+};
 </script>
